6c55dceab6b8609b0a0b244c48ad864bebbfef67
gisacc54 <giftisacc54@gmail.com>
2024-06-12 17:12:29 +0300

    first commit

--- app/src/main/java/com/example/myapplication23/MainActivity.java
+++ app/src/main/java/com/example/myapplication23/MainActivity.java
@@ -1,15 +1,7 @@
 package com.example.myapplication23;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,15 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView textView = findViewById(R.id.textView);
-        EditText editText = findViewById(R.id.editTextText);
-        Button button = findViewById(R.id.button);
-
-        button.setOnClickListener(v -> {
-            String text = editText.getText().toString();
-            textView.setText(text);
-        });
-
     }
 }
